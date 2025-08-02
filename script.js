5fda4ef30fb00bcee68c6665991db3c3a800f9f3
Chet07-R <178676635+Chet07-R@users.noreply.github.com>
2025-08-03 00:00:01 +0530

    Add a JS file, links nav and footer on all pages
    So basically filling the codebase with the same code of the reused compoonents like nav and footer, I made a script that will load the reused contents on the pages we need by just using the class name in html

---
+++ script.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('.navbar')
+const bottom = document.querySelector('.footer')    
+fetch('./navbar.html')
+.then(res => res.text())
+.then(data =>{
+    nav.innerHTML = data
+    const parser = new DOMParser();   // this is is the file we are including also has a javacript included in it
+    const doc = parser.parseFromString(data, 'text/html');  // this will accept two parameters and text/html - type of content we need
+    eval(doc.querySelector('script').textContent)  // eval will execute if the content is js 
+
+})
+
+fetch('./footer.html')
+.then(res => res.text())
+.then(data =>{
+    bottom.innerHTML = data
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'text/html');  
+    eval(doc.querySelector('script').textContent)  
+
+})
