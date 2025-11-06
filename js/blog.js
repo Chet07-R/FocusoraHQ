f670957935733ab3309606954d1e03cbe21c9718
Vansh Thakur <vthakur.290905@gmail.com>
2025-11-06 11:30:04 +0530

    added blog css and js

--- js/blog.js
+++ js/blog.js
@@ -18,3 +18,35 @@ loadMoreBtn.addEventListener('click', function() {
         firstNewArticle.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }, 100);
 });
+
+
+// For sidebar to be coloured when on a specific anchor tag 
+const tocLinks = document.querySelectorAll('.toc-link');
+  const sections = [];
+
+  // Collect all sections
+  tocLinks.forEach(link => {
+    const id = link.getAttribute('href').substring(1);
+    const section = document.getElementById(id);
+    if (section) {
+      sections.push({ id: id, element: section });
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+    
+    sections.forEach(section => {
+      const sectionTop = section.element.offsetTop - 150;
+      if (window.scrollY >= sectionTop) {
+        current = section.id;
+      }
+    });
+    
+    tocLinks.forEach(link => {
+      link.classList.remove('active-toc');
+      if (link.getAttribute('href') === '#' + current) {
+        link.classList.add('active-toc');
+      }
+    });
+  });
